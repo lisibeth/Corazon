e13b42f4cbc214f4e548d85a10f040bf4c6f1f1e
lisibeth <lisibeth20@gmail.com>
2024-05-25 21:28:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const heart = document.getElementById('heart');
+const arrow = document.querySelector('.arrow');
+
+/*
+arrow.addEventListener('animationiteration', () => {
+    setTimeout(() => {
+        heart.classList.add('cracked');
+    }, 1800); // Ajusta este valor para sincronizar con el momento en que la flecha pasa sobre el corazón
+    setTimeout(() => {
+        heart.classList.remove('cracked');
+    }, 2300); // Ajusta este valor para finalizar la grieta
+}); */
